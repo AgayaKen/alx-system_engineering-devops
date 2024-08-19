158fc0b2171a5e82a1c99017d53f092e241668f3
AgayaKen <kennedyagaya@gmail.com>
2024-08-19 11:28:44 -0700

    Querrying Reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API and returns the number of subscribers
+for a given subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers('python'))  # Replace 'python' with any subreddit name
