3f48e39d1abff5d4e4ffd11b4c33c8ae54cef503
AgayaKen <kennedyagaya@gmail.com>
2024-08-19 09:49:50 -0700

    Puppet code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,5 @@
+# This Puppet script creates a file at /tmp/school with specific content, permissions, and ownership
+
 file { '/tmp/school':
   ensure  => 'file',
   content => 'I love Puppet',
@@ -5,3 +7,4 @@ file { '/tmp/school':
   owner   => 'www-data',
   group   => 'www-data',
 }
+
