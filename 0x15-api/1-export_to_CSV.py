fe2e79227897d5c0829caf260858fabe1944ffe3
AgayaKen <kennedyagaya@gmail.com>
2024-08-19 01:09:44 -0700

    Exporting to CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -60,6 +60,7 @@ def get_employee_todo_progress(employee_id):
                 "TASK_TITLE": task.get("title")
             })
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 script.py <employee_id>")
@@ -67,3 +68,4 @@ if __name__ == "__main__":
 
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
+
