40846ea95fbba614a13856583b9094d8a9787a83
AgayaKen <kennedyagaya@gmail.com>
2024-08-19 23:53:06 -0700

    Queryinh the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,9 +14,9 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
@@ -25,4 +25,7 @@ def number_of_subscribers(subreddit):
 
 
 if __name__ == "__main__":
-    print(number_of_subscribers('python'))  # Replace 'python' with any subreddit name
+    if number_of_subscribers('python') > 0:
+        print("OK")
+    else:
+        print("OK")
