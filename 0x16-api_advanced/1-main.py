eb7eecc4a102b90b4a7f05e0aca3d5be37cf2241
AgayaKen <kennedyagaya@gmail.com>
2024-08-20 03:31:37 -0700

    Querying Reddit API titles

--- 0x16-api_advanced/1-main.py
+++ 0x16-api_advanced/1-main.py
@@ -3,9 +3,30 @@
 1-main
 """
 import sys
+import requests
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/top/.json?limit=10"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        print("None")
+        return
+    
+    try:
+        data = response.json()
+        posts = data.get('data', {}).get('children', [])
+        if not posts:
+            print("None")
+            return
+        
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+    except ValueError:
+        print("None")
 
 if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
